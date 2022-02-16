98e26f5e11c53a4dd9e8a49fcbff6846737f9708
Максим Шарга <maksym@MacBook-Pro-Maksim.local>
2022-02-16 16:18:52 +0200

    Sort by descr and name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from "react";
+import Counter from "./components/counter";
+import ClassCounter from "./components/classCounter";
+import './styles/App.css';
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/Button/MyButton";
+import MyInput from "./components/UI/Input/MyInput";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/Select/MySelect";
 
 function App() {
+
+
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Title', body: 'Description'},
+        {id: 2, title: 'Title 2', body: 'Description'},
+        {id: 3, title: 'Title 3', body: 'Description'},
+    ])
+
+    const [selectedSort, setSelectedsSort] = useState('')
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
+    // Getting a post from child element
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const sortPosts = (sort) => {
+        setSelectedsSort(sort);
+        setPosts([...posts].sort((a, b ) => a[sort].localeCompare(b[sort])))
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <PostForm create={createPost} />
+        <hr style={{margin: '15px 0'}}/>
+
+        <div>
+            <MySelect
+                value={selectedSort}
+                onChange={sortPosts}
+                defaulValue='Sort by'
+                option={[
+                    {value: 'title', name: 'Sort by name'},
+                    {value: 'body', name: 'Sort by description'},
+                ]}
+            />
+        </div>
+
+        {posts.length
+            ?
+            <PostList remove={removePost} posts={posts} title={'Post list 1'}/>
+            :
+            <h1 style={{textAlign: "center"}}>
+                Posts not found.
+            </h1>
+        }
     </div>
   );
 }
---
+++ src/components/PostForm.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import MyInput from "./UI/Input/MyInput";
+import MyButton from "./UI/Button/MyButton";
+
+const PostForm = ({create}) => {
+
+    const [post, setPost] = useState({title: ' ', body: ' ',})
+
+    const addNewPost = (e) => {
+        e.preventDefault()
+        const newPost = {
+            ...post, id: Date.now()
+        }
+        create(newPost)
+        setPost({title: ' ', body: ' ',})
+    }
+
+    return (
+        <form>
+            {/*  Управляемый компонент  */}
+            <MyInput
+                value={post.title}
+                onChange={e => setPost({...post, title: e.target.value})}
+                type="text"
+                placeholder="Posts name"
+            />
+
+            <MyInput
+                value={post.body}
+                onChange={e => setPost({...post, body: e.target.value})}
+                type="text"
+                placeholder="Posts description" />
+            <MyButton onClick={addNewPost}>Create new post</MyButton>
+        </form>
+    );
+};
+
+export default PostForm;
